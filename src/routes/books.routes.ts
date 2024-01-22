fd279bdf784152e0a878d1375617a528aca63f56
Yann G. Simonetti Buffet <yanngsb@hotmail.com>
2024-01-22 17:43:21 -0300

    feat: implemented validation in post and update requests

--- src/routes/books.routes.ts
+++ src/routes/books.routes.ts
@@ -2,6 +2,8 @@ import { Router } from "express";
 import { BooksControllers } from "../controllers/books.controllers";
 import { IsBookIdValid } from "../middlewares/isBookIdValid.middleware";
 import { IsBookAlreadyRegistered } from "../middlewares/isBookAlreadyRegistered.middleware";
+import { RequestValidation } from "../middlewares/requestValidation.middleware";
+import { addBookSchema, updateBookSchema } from "../schemas/book.schema";
 
 export const booksRouter = Router();
 
@@ -9,15 +11,21 @@ const booksControllers = new BooksControllers();
 
 booksRouter.post(
   "/",
+  RequestValidation.execute({ body: addBookSchema }),
   IsBookAlreadyRegistered.execute,
   booksControllers.addBook
 );
+
 booksRouter.get("/", booksControllers.getAll);
+
 booksRouter.get("/:id", IsBookIdValid.execute, booksControllers.getById);
+
 booksRouter.patch(
   "/:id",
+  RequestValidation.execute({ body: updateBookSchema }),
   IsBookIdValid.execute,
   IsBookAlreadyRegistered.execute,
   booksControllers.updateBook
 );
+
 booksRouter.delete("/:id", IsBookIdValid.execute, booksControllers.deleteBook);
